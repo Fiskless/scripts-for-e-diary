2803255d89a04777e43195d479fc9da607b32a37
Fiskless <70478482+Fiskless@users.noreply.github.com>
2020-11-10 19:24:35 +0500

    Add try-except and PEP8

--- scripts.py
+++ scripts.py
@@ -3,7 +3,6 @@ from datacenter.models import Lesson
 from datacenter.models import Commendation
 from datacenter.models import Chastisement
 from datacenter.models import Mark
-from django.http import Http404
 import random
 
 
@@ -11,41 +10,58 @@ def fix_marks(schoolkid_name):
 
     try:
         child = Schoolkid.objects.get(full_name__contains=schoolkid_name)
+        bad_marks = Mark.objects.filter(schoolkid=child, points__in=[2, 3])
+        for bad_mark in bad_marks:
+            bad_mark.points = 5
+            bad_mark.save()
     except Schoolkid.DoesNotExist:
-        raise Http404("Wrong schoolkid name")
-
-    bad_marks = Mark.objects.filter(schoolkid=child, points__in=[2, 3])
-    for bad_mark in bad_marks:
-        bad_mark.points = 5
-        bad_mark.save()
+        print("Wrong schoolkid name")
+    except Schoolkid.MultipleObjectsReturned:
+        print("Wrong schoolkid name")
 
 
 def remove_chastisements(schoolkid_name):
 
     try:
-        schoolkid = Schoolkid.objects.get(full_name__contains=schoolkid_name, )
+        schoolkid = Schoolkid.objects.get(full_name__contains=schoolkid_name)
+        chastisements = Chastisement.objects.filter(schoolkid=schoolkid)
+        chastisements.delete()
     except Schoolkid.DoesNotExist:
-        raise Http404("Wrong schoolkid name")
-
-    chastisements = Chastisement.objects.filter(schoolkid=schoolkid)
-    chastisements.delete()
+        print("Wrong schoolkid name")
+    except Schoolkid.MultipleObjectsReturned:
+        print("Wrong schoolkid name")
 
 
 def create_commendation(schoolkid_name, lesson, year_of_study, group_letter):
-    lessons = Lesson.objects.filter(year_of_study=year_of_study, group_letter=group_letter, subject__title__contains=lesson).order_by('-subject').first()
-    child = Schoolkid.objects.get(full_name__contains=schoolkid_name)
-    commendation_list=['Молодец!',
-                       'Отлично!',
-                       'Хорошо!',
-                       ' Гораздо лучше, чем я ожидал!',
-                       'Ты меня приятно удивил!',
-                       'Ты, как всегда, точен!',
-                       'Талантливо!','Потрясающе!',
-                       'Замечательно!',
-                       'Так держать!',
-                       'Здорово!',
-                       'Я тобой горжусь!',
-                       'Ты растешь над собой!']
-    commendation_text = random.choice(commendation_list)
-    Commendation.objects.create(text=commendation_text, created=lessons.date, schoolkid=child, subject=lessons.subject, teacher=lessons.teacher)
 
+    try:
+        lessons = Lesson.objects.filter(
+            year_of_study=year_of_study,
+            group_letter=group_letter,
+            subject__title__contains=lesson).order_by('-subject').first()
+        child = Schoolkid.objects.get(full_name__contains=schoolkid_name)
+        commendation_list = ['Молодец!',
+                             'Отлично!',
+                             'Хорошо!',
+                             ' Гораздо лучше, чем я ожидал!',
+                             'Ты меня приятно удивил!',
+                             'Ты, как всегда, точен!',
+                             'Талантливо!',
+                             'Потрясающе!',
+                             'Замечательно!',
+                             'Так держать!',
+                             'Здорово!',
+                             'Я тобой горжусь!',
+                             'Ты растешь над собой!']
+        commendation_text = random.choice(commendation_list)
+        Commendation.objects.create(text=commendation_text,
+                                    created=lessons.date,
+                                    schoolkid=child,
+                                    subject=lessons.subject,
+                                    teacher=lessons.teacher)
+    except Schoolkid.DoesNotExist:
+        print("Wrong schoolkid name")
+    except Schoolkid.MultipleObjectsReturned:
+        print("Wrong schoolkid name")
+    except AttributeError:
+        print("Wrong year of study, group letter or subject title")
